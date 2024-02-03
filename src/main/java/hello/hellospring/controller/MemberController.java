f2a3ffac99a92f13dbef90d1b36c84a1924cd1fd
jin <hurjin1109@naver.com>
2024-02-03 22:41:28 +0900

    test

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -5,8 +5,12 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -19,7 +23,8 @@ public class MemberController {
     }
 
     @GetMapping("/members/new")
-    public String createForm(){
+    public String createForm() {
+
         return "members/createMemberForm";
     }
 
@@ -28,13 +33,19 @@ public class MemberController {
         Member member = new Member();
         member.setName(form.getName());
 
+
         memberService.join(member);
+
+
         //홈 화면으로 보낸느거
         return "redirect:/";
 
-
-
     }
 
-
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
